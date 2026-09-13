466e5e047dbc87d26591f4957bfa8565139d5fde
snoopryees <nhong11@my.bcit.ca>
2026-09-13 11:17:47 -0700

    Add initial project structure, UI layout, and external strings for lab 1

---
+++ COMP4537/labs/1/lang/messages/en/user.js
@@ -0,0 +1,7 @@
+export const STRINGS = {
+    PROMPT: "How many buttons to create?",
+    GO_BTN: "Go!",
+    WIN: "Excellent memory!",
+    LOSE: "Wrong order!",
+    ERROR: "Please enter a valid number (3-7)."
+};
