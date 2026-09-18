2928164ae503a324b3af4757816e2363fa08ecf7
snoopryees <nhong11@my.bcit.ca>
2026-09-18 12:59:39 -0700

    add Note class

---
+++ COMP4537/labs/2/js/Note.js
@@ -0,0 +1,9 @@
+/**
+ * Note class - represents a single note object.
+ * Used to form the array of objects stored in localStorage.
+ */
+class Note {
+  constructor(text = "") {
+    this.text = text;
+  }
+}
